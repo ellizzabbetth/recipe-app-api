89922478eab1ccf049b24c01d984028f334e3674
eli <ellizzabbetth@gmail.com>
2022-01-03 18:17:25 -0800

    added custom recipe

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -30,6 +30,7 @@ class RecipeSerializer(serializers.ModelSerializer):
         queryset=Tag.objects.all()
     )
 
+
     class Meta:
         model = Recipe
         fields = (
@@ -37,3 +38,8 @@ class RecipeSerializer(serializers.ModelSerializer):
             'price', 'link'
         )
         read_only_fields = ('id',)
+
+class RecipeDetailSerializer(RecipeSerializer):
+    """Serialize a recipe detail"""
+    ingredients = IngredientSerializer(many=True, read_only=True)
+    tags = TagSerializer(many=True, read_only=True)
