b5c86b6796ef198c128be0b98f48097548633257
eli <ellizzabbetth@gmail.com>
2022-01-07 05:29:14 -0800

    authorization and authentication

--- app/user/serializers.py
+++ app/user/serializers.py
@@ -26,7 +26,7 @@ class UserSerializer(serializers.ModelSerializer):
             user.save()
 
         return user
-        
+
 class AuthTokenSerializer(serializers.Serializer):
     """Serializer for the user authentication object"""
     email = serializers.CharField()
